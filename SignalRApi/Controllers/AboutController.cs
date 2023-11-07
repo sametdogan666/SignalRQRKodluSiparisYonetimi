2dc108cb6189ef896b4ab51347917a85b7d97f5b
Samet DOGAN <sametdogan23@protonmail.ch>
2023-11-07 18:11:48 +0300

    about api controller

---
+++ SignalRApi/Controllers/AboutController.cs
@@ -0,0 +1,77 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SignalR.Business.Abstract;
+using SignalR.Dto.AboutDto;
+using SignalR.Entities.Entities;
+
+namespace SignalRApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AboutController : ControllerBase
+    {
+        private readonly IAboutService _aboutService;
+
+        public AboutController(IAboutService aboutService)
+        {
+            _aboutService = aboutService;
+        }
+
+        [HttpGet("get-list-abouts")]
+        public IActionResult GetListAbouts()
+        {
+            var results = _aboutService.GetAll();
+
+            return Ok(results);
+        }
+
+        [HttpPost("create-about")]
+        public IActionResult CreateAbout(CreateAboutDto createAboutDto)
+        {
+            About about = new About()
+            {
+                Title = createAboutDto.Title,
+                Description = createAboutDto.Description,
+                ImageUrl = createAboutDto.ImageUrl
+            };
+
+            _aboutService.Add(about);
+
+            return Ok("Hakkımda Kısmı Başarılı Bir Şekilde Eklendi");
+        }
+
+        [HttpDelete("delete-about/{id}")]
+        public IActionResult DeleteAbout(int id)
+        {
+            var value = _aboutService.GetById(id);
+
+            _aboutService.Remove(value);
+
+            return Ok("Hakkımda Alanı Başarılı Bir Şekilde Silindi");
+        }
+
+        [HttpPut("update-about")]
+        public IActionResult UpdateAbout(UpdateAboutDto updateAboutDto)
+        {
+            About about = new About()
+            {
+                Id = updateAboutDto.Id,
+                Title = updateAboutDto.Title,
+                Description = updateAboutDto.Description,
+                ImageUrl = updateAboutDto.ImageUrl
+            };
+
+            _aboutService.Update(about);
+
+            return Ok("Hakkımda Alanı Başarılı Bir Şekilde Güncellendi");
+        }
+
+        [HttpGet("get-by-id-about/{id}")]
+        public IActionResult GetByIdAbout(int id)
+        {
+            var result = _aboutService.GetById(id);
+
+            return Ok(result);
+        }
+    }
+}
--- SignalRApi/Program.cs
+++ SignalRApi/Program.cs
@@ -1,11 +1,48 @@
+using Microsoft.OpenApi.Models;
+using SignalR.Business.Abstract;
+using SignalR.Business.Concrete;
+using SignalR.DataAccess.Abstract;
+using SignalR.DataAccess.Concrete;
+using SignalR.DataAccess.EntityFramework;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+builder.Services.AddDbContext<SignalRContext>();
+
+builder.Services.AddScoped<IAboutService, AboutManager>();
+builder.Services.AddScoped<IAboutDal, EfAboutDal>();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+//builder.Services.AddSwaggerGen(c =>
+//{
+//    c.SwaggerDoc("v1", new OpenApiInfo { Title = "SignalRQRProject", Version = "v1" });
+//    c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+//    {
+//        In = ParameterLocation.Header,
+//        Description = "Please Insert Token",
+//        Name = "Authorization",
+//        Type = SecuritySchemeType.Http,
+//        BearerFormat = "JWT",
+//        Scheme = "bearer"
+//    });
+//    c.AddSecurityRequirement(new OpenApiSecurityRequirement
+//    {
+//        {
+//            new OpenApiSecurityScheme
+//            {
+//                Reference = new OpenApiReference
+//                {
+//                    Type = ReferenceType.SecurityScheme,
+//                    Id = "Bearer"
+//                }
+//            },
+//            new string[]{}
+//        }
+//    });
+//});
 
 var app = builder.Build();
 
