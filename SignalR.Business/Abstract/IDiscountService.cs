ee902143b0e2a474e51c52d6de3e0189d8b108a3
Samet DOGAN <sametdogan23@protonmail.ch>
2023-11-07 17:01:23 +0300

    business abstract services

---
+++ SignalR.Business/Abstract/IDiscountService.cs
@@ -0,0 +1,8 @@
+﻿using SignalR.Entities.Entities;
+
+namespace SignalR.Business.Abstract;
+
+public interface IDiscountService : IGenericService<Discount>
+{
+
+}
---
+++ SignalR.Business/Abstract/IGenericService.cs
@@ -0,0 +1,10 @@
+﻿namespace SignalR.Business.Abstract;
+
+public interface IGenericService<T> where T : class
+{
+    void Add(T entity);
+    void Remove(T entity);
+    void Update(T entity);
+    T? GetById(int id);
+    List<T> GetAll();
+}
