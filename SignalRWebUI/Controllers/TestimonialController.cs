6a66b486ee25e6ca1489484ccb92fc553e9417d2
Samet DOGAN <sametdogan23@protonmail.ch>
2023-11-08 13:09:17 +0300

    testimonial api consume

---
+++ SignalRWebUI/Controllers/TestimonialController.cs
@@ -0,0 +1,101 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using SignalRWebUI.ViewModels.CategoryViewModels;
+using System.Text;
+using SignalRWebUI.ViewModels.TestimonialViewModels;
+
+namespace SignalRWebUI.Controllers;
+
+public class TestimonialController : Controller
+{
+    private readonly IHttpClientFactory _httpClientFactory;
+
+    public TestimonialController(IHttpClientFactory httpClientFactory)
+    {
+        _httpClientFactory = httpClientFactory;
+    }
+
+    public async Task<IActionResult> Index()
+    {
+        var client = _httpClientFactory.CreateClient();
+        var responseMessage = await client.GetAsync("https://localhost:7065/api/Testimonial/get-list-testimonials");
+
+        if (responseMessage.IsSuccessStatusCode)
+        {
+            var jsonData = await responseMessage.Content.ReadAsStringAsync();
+            var results = JsonConvert.DeserializeObject<List<ResultTestimonialViewModel>>(jsonData);
+
+            return View(results);
+        }
+
+        return View();
+    }
+
+    [HttpGet]
+    public IActionResult CreateTestimonial()
+    {
+        return View();
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> CreateTestimonial(CreateTestimonialViewModel createTestimonialViewModel)
+    {
+        var client = _httpClientFactory.CreateClient();
+        var jsonData = JsonConvert.SerializeObject(createTestimonialViewModel);
+        StringContent stringContent = new StringContent(jsonData, Encoding.UTF8, "application/json");
+        var responseMessage = await client.PostAsync("https://localhost:7065/api/Testimonial/create-testimonial", stringContent);
+
+        if (responseMessage.IsSuccessStatusCode)
+        {
+            return RedirectToAction("Index");
+        }
+
+        return View();
+    }
+
+    public async Task<IActionResult> DeleteTestimonial(int id)
+    {
+        var client = _httpClientFactory.CreateClient();
+        var responseMessage = await client.DeleteAsync($"https://localhost:7065/api/Testimonial/delete-testimonial/{id}");
+
+        if (responseMessage.IsSuccessStatusCode)
+        {
+            return RedirectToAction("Index");
+        }
+
+        return View();
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> UpdateTestimonial(int id)
+    {
+        var client = _httpClientFactory.CreateClient();
+        var responseMessage = await client.GetAsync($"https://localhost:7065/api/Testimonial/get-by-id-testimonial/{id}");
+
+        if (responseMessage.IsSuccessStatusCode)
+        {
+            var jsonData = await responseMessage.Content.ReadAsStringAsync();
+            var value = JsonConvert.DeserializeObject<UpdateTestimonialViewModel>(jsonData);
+
+            return View(value);
+        }
+
+        return View();
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> UpdateTestimonial(UpdateTestimonialViewModel updateTestimonialViewModel)
+    {
+        var client = _httpClientFactory.CreateClient();
+        var jsonData = JsonConvert.SerializeObject(updateTestimonialViewModel);
+        StringContent stringContent = new StringContent(jsonData, Encoding.UTF8, "application/json");
+        var responseMessage = await client.PutAsync("https://localhost:7065/api/Testimonial/update-testimonial", stringContent);
+
+        if (responseMessage.IsSuccessStatusCode)
+        {
+            return RedirectToAction("Index");
+        }
+
+        return View();
+    }
+}
---
+++ SignalRWebUI/ViewModels/TestimonialViewModels/CreateTestimonialViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace SignalRWebUI.ViewModels.TestimonialViewModels;
+
+public class CreateTestimonialViewModel
+{
+    public string? FullName { get; set; }
+    public string? Title { get; set; }
+    public string? Comment { get; set; }
+    public string? ImageUrl { get; set; }
+    public bool Status { get; set; }
+}
