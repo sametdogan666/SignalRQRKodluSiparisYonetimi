1e42e835b58df72653791026ca22fb3061251240
Samet DOGAN <sametdogan23@protonmail.ch>
2023-11-08 13:42:50 +0300

    signalR installation completed

---
+++ SignalRApi/Hubs/SignalRHub.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace SignalRApi.Hubs;
+
+//Hub sınıfı bir sunucu görevi görüyor. Dağıtım işlemleri buradan yapılır
+public class SignalRHub : Hub
+{
+
+}
--- SignalRApi/Program.cs
+++ SignalRApi/Program.cs
@@ -6,8 +6,21 @@ using SignalR.Business.Concrete;
 using SignalR.DataAccess.Abstract;
 using SignalR.DataAccess.Concrete;
 using SignalR.DataAccess.EntityFramework;
+using SignalRApi.Hubs;
 
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddCors(opt =>
+{
+    opt.AddPolicy("CorsPolicy", builder =>
+    {
+        builder.AllowAnyHeader()
+            .AllowAnyMethod()
+            .SetIsOriginAllowed((host) => true)
+            .AllowCredentials();
+    });
+});
+builder.Services.AddSignalR();
 
 builder.Services.AddDbContext<SignalRContext>();
 builder.Services.AddAutoMapper(Assembly.GetExecutingAssembly());
@@ -83,10 +96,14 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
+app.UseCors("CorsPolicy");
+
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
 
 app.MapControllers();
+
+app.MapHub<SignalRHub>("/signalrhub");
 
 app.Run();
