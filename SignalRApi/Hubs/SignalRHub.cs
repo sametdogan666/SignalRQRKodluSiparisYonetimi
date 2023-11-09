9316872e614f4f38cf47d8652dceb9ac6bda6b87
Samet DOGAN <sametdogan23@protonmail.ch>
2023-11-09 18:32:01 +0300

    signalR configuration of statistics

--- SignalRApi/Hubs/SignalRHub.cs
+++ SignalRApi/Hubs/SignalRHub.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.SignalR;
+using SignalR.Business.Abstract;
 using SignalR.DataAccess.Concrete;
 
 namespace SignalRApi.Hubs;
@@ -6,11 +7,24 @@ namespace SignalRApi.Hubs;
 //Hub sınıfı bir sunucu görevi görüyor. Dağıtım işlemleri buradan yapılır
 public class SignalRHub : Hub
 {
-    SignalRContext _context = new SignalRContext();
+    private readonly ICategoryService _categoryService;
+    private readonly IProductService _productService;
+
+    public SignalRHub(ICategoryService categoryService, IProductService productService)
+    {
+        _categoryService = categoryService;
+        _productService = productService;
+    }
 
     public async Task SendCategoryCount()
     {
-        var results = _context.Categories.Count();
-        await Clients.All.SendAsync("ReceiveCategoryCount", results);
+        var resultCategoryCount = _categoryService.GetCategoryCount();
+        await Clients.All.SendAsync("ReceiveCategoryCount", resultCategoryCount);
+    }
+
+    public async Task SendProductCount()
+    {
+        var resultProductCount = _productService.GetProductCount();
+        await Clients.All.SendAsync("ReceiveProductCount", resultProductCount);
     }
 }
