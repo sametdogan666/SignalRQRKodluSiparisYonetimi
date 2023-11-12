7b19b4634ba69a1f44691582f8059606ae4cc613
Samet DOGAN <sametdogan23@protonmail.ch>
2023-11-12 04:53:13 +0300

    booking process

---
+++ SignalRWebUI/Controllers/BookATableController.cs
@@ -0,0 +1,38 @@
+﻿using System.Text;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using SignalRWebUI.ViewModels.BookingViewModel;
+
+namespace SignalRWebUI.Controllers;
+
+public class BookATableController : Controller
+{
+    private readonly IHttpClientFactory _httpClientFactory;
+
+    public BookATableController(IHttpClientFactory httpClientFactory)
+    {
+        _httpClientFactory = httpClientFactory;
+    }
+
+    [HttpGet]
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Index(CreateBookingViewModel createBookingViewModel)
+    {
+        var client = _httpClientFactory.CreateClient();
+        var jsonData = JsonConvert.SerializeObject(createBookingViewModel);
+        StringContent stringContent = new StringContent(jsonData, Encoding.UTF8, "application/json");
+        var responseMessage = await client.PostAsync("https://localhost:7065/api/Booking/create-booking",stringContent);
+
+        if (responseMessage.IsSuccessStatusCode)
+        {
+            return RedirectToAction("Index", "Default");
+        }
+
+        return View();
+    }
+}
