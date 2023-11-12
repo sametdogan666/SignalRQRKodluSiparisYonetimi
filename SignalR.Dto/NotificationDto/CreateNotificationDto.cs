5a705951bf62b897239996ab5a4b6fa985bb4875
Samet DOGAN <sametdogan23@protonmail.ch>
2023-11-12 18:22:01 +0300

    notification delete, update, create transactions

--- SignalR.Dto/NotificationDto/CreateNotificationDto.cs
+++ SignalR.Dto/NotificationDto/CreateNotificationDto.cs
@@ -5,6 +5,4 @@ public class CreateNotificationDto
     public string? Type { get; set; }
     public string? Icon { get; set; }
     public string? Description { get; set; }
-    public DateTime Date { get; set; }
-    public bool Status { get; set; }
 }
--- SignalRWebUI/ViewModels/NotificationViewModels/CreateNotificationViewModel.cs
+++ SignalRWebUI/ViewModels/NotificationViewModels/CreateNotificationViewModel.cs
@@ -5,6 +5,4 @@ public class CreateNotificationViewModel
     public string? Type { get; set; }
     public string? Icon { get; set; }
     public string? Description { get; set; }
-    public DateTime Date { get; set; }
-    public bool Status { get; set; }
 }
